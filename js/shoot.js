3e44eefb0eab297a527bc056b97ad52438dff85f
Nathan Arsement <nathanarsement@gmail.com>
2023-07-30 13:09:39 +0530

    Add files via upload

--- js/shoot.js
+++ js/shoot.js
@@ -11,6 +11,7 @@ AFRAME.registerComponent("bullets",{
                 bullet.setAttribute('scale',"4 4 4")
                 bullet.setAttribute("material","color","black")
                 bullet.setAttribute("dynamic-body", { shape: "sphere", sphereRadius: 0.5, mass: 50 })
+                bullet.addEventListener("collide", this.removeBullet)
                 var cam = document.querySelector("#camera")
                 pos=cam.getAttribute("position")
                 pos1=pos.y-0.8
@@ -27,5 +28,32 @@ AFRAME.registerComponent("bullets",{
                 scene.appendChild(bullet)
             }
         })
+    },
+    removeBullet: function (e) {
+        // console.log(e.detail.target.el)
+        // console.log(e.detail.body.el)
+        var element = e.detail.target.el
+        var elementHit = e.detail.body.el
+        if (elementHit.id.includes("pin")) {
+            elementHit.setAttribute("material", {
+                opacity: 0,
+                transparent: true
+            })
+            var impulse = new CANNON.Vec3(0,1,-15)
+            var worldPoint = new CANNON.Vec3().copy(
+                elementHit.getAttribute("position")
+            )
+            elementHit.body.applyImpulse(impulse, worldPoint)
+            element.removeEventListener("collide", this.removeBullet)
+            
+            setTimeout(function(){
+                var scene = document.querySelector("#pinball")
+                scene.removeChild(elementHit)
+                var scene = document.querySelector("#scene")
+                scene.removeChild(element)
+            },5000)
+            
+        }
+
     }
 })
